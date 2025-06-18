a9a0033282738770ba8fddd8f0dffd58a2123919
Gowtham <gowtham.s@theporter.in>
2025-06-18 17:41:19 +0530

    branch-2 >> first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to React!
+          Welcome to React! from branch 2
         </a>
       </header>
     </div>
