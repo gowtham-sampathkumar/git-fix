7e800f42e9ef26a78c2af7e83182248a44b6422f
Gowtham <gowtham.s@theporter.in>
2025-06-19 15:15:11 +0530

    map method removed from root component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ const App = () => {
     <>
       <h1>Welcome to My App</h1>
       <p>example text for commit message</p>
-      {[].map((item, index) => (
-        <div key={index}>
-          <p>Item {index + 1}</p>
-        </div>
-      ))}
-      <p>End of the list</p>
     </>
   )
 }
